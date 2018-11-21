e83fa59021c89f994ee7c57630336922008b0e6b
roberts98 <robertsidzinka@interia.pl>
2018-11-21 13:40:01 +0100

    Removing static data

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 500, createdAt: 500}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 35, createdAt: 19500}));
-store.dispatch(addExpense({ description: 'Rent', amount: 800, createdAt: 1900}));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
